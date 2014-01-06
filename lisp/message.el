1d2e6ea95b8484b4cb63db06b9179cd676be3ac7
Paul Eggert <eggert@cs.ucla.edu>
2014-01-06 06:53:43 +0000

    message.el (message-recover): Spelling fix

--- lisp/message.el
+++ lisp/message.el
@@ -7258,7 +7258,7 @@ header line with the old Message-ID."
 	   (let ((buffer-read-only nil))
 	     (erase-buffer)
 	     (insert-file-contents file-name nil)))
-	  (t (error "message-recover cancelled")))))
+	  (t (error "message-recover canceled")))))
 
 ;;; Washing Subject:
 
