d8774ba54f1a28d3246e0f541b023dd91c0380fd
Lars Ingebrigtsen <larsi@building.gnus.org>
2014-01-31 15:02:31 -0800

    Ma Gnus v0.10 is begun

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -294,7 +294,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "0.9"
+(defconst gnus-version-number "0.10"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Ma Gnus v%s" gnus-version-number)
