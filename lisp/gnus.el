286dc18fd9d9f1d9f215ce3e7b1a4abe788d401e
Lars Magne Ingebrigtsen <larsi@gnus.org>
2014-05-01 19:44:35 +0200

    Ma Gnus v0.11 is released

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -290,7 +290,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "0.10"
+(defconst gnus-version-number "0.11"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Ma Gnus v%s" gnus-version-number)
